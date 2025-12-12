0a658a5ee3fe6f95e4ddcbcd35d73c6e1e00f3d7
Jaydev Parmar <jydev@duck.com>
2025-12-12 18:26:05 +0530

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
