bc9be789031467fc26f92a7a36551cc0a16ea0a8
Jaydev Parmar <jydev@duck.com>
2026-01-12 17:44:40 +0530

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://hackclubaps.vercel.app",
   output: "static",
   integrations: [
     react(),
+    sitemap(),
     tailwind(),
     vercel({
       webAnalytics: {
